e1bdace1f879f5cf9345a55ebbd0dec1ef99b558
Beck20003 <beckburnham03@gmail.com>
2024-04-18 09:50:16 -0600

    created angular app and added firebase

---
+++ angular-project/src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyBEaNcoARhaaugJ7B56awRpQteAQxhB09g",
+        authDomain: "angularfire-4879f.firebaseapp.com",
+        projectId: "angularfire-4879f",
+        storageBucket: "angularfire-4879f.appspot.com",
+        messagingSenderId: "394748093272",
+        appId: "1:394748093272:web:3c98685f961a8f7d34bb2d"
+    }
+  };
